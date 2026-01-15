e3b7e12811159ec419607574e7b23087e3504d14
Charan <121456054+Charan-place@users.noreply.github.com>
2026-01-15 21:04:15 +0530

    Time: 2 ms (99.17%) | Memory: 72.6 MB (17.90%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int s=0;
+        int s5=0;
+        int s10=0;
+        int s20=0;
+        // Arrays.sort(bills);
+        for(int i=0;i<bills.length;i++){
+            if(bills[i]==5){
+                s5++;
+            }
+            else if(bills[i]==10){
+                if(s5>0){
+                    s5--;
+                    s10++;
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                if(s5>0){
+                    if(s10>0){
+                        s20++;
+                        s10--;
+                        s5--;
+                    }
+                    else if(s5>=3){
+                        s5=s5-3;
+                        s20++;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
